d31503c19c062c4224da66d72010ecff2d70b5b7
Daniel Ince <incynerated@gmail.com>
2022-09-05 20:24:29 +0100

    websocket

--- tickchart.py
+++ tickchart.py
@@ -1,2 +1,12 @@
-print("hello world")
-print("blah blah")
+import unicorn_binance_websocket_api
+import numpy as np
+
+ubwa = unicorn_binance_websocket_api.BinanceWebSocketApiManager(exchange="binance.com")
+ubwa.create_stream(['trade'], ['ethusdt'], output="UnicornFy")
+
+while True:
+    oldest_data_from_stream_buffer = ubwa.pop_stream_data_from_stream_buffer()
+    if oldest_data_from_stream_buffer:
+        price = np.array(oldest_data_from_stream_buffer.get('price'))
+        quantity = np.array(oldest_data_from_stream_buffer.get('quantity'))
+        print(f"{price},{quantity}")
