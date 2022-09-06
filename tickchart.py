83b2a4080235158e1db7d9e4b78a7159dbda35bf
Daniel Ince <incynerated@gmail.com>
2022-09-06 12:16:18 +0100

    working

--- tickchart.py
+++ tickchart.py
@@ -12,10 +12,9 @@ timedata = []
 
 def update_graph():
     ax.plot(timedata, pricedata, color='red')
-    ax.legend(f"Last price: {pricedata[-1]}")
 
     fig.canvas.draw()
-    plt.pause(0.1)
+    plt.pause(0.01)
 
 async def main():
     ubwa = unicorn_binance_websocket_api.BinanceWebSocketApiManager(exchange="binance.com")
@@ -25,8 +24,18 @@ async def main():
         if oldest_data_from_stream_buffer:
             price = np.array(oldest_data_from_stream_buffer.get('price'))
             trade_time = np.array(oldest_data_from_stream_buffer.get('trade_time'))
-            pricedata.append(price)
-            timedata.append(trade_time)
+
+            if price == None:
+                pass
+            else:
+                pricedata.append(price)
+
+            if trade_time == None:
+                pass
+            else:
+                timedata.append(trade_time)
+
+            print(price, trade_time)
 
             update_graph()
 
